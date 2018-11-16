95091f03be9a0617cc3a7677644e6e8b69b1b6b6
alekseyshmat <leha.2409@mail.ru>
2018-11-16 23:09:19 +0300

    changed smartPointer

--- Lab2/SmartPointer.h
+++ Lab2/SmartPointer.h
@@ -1,7 +1,5 @@
 #include <Windows.h>
 
-using namespace std;
-
 namespace SmartPointer
 {
 	struct ref_count_data {
@@ -12,37 +10,34 @@ namespace SmartPointer
 	{
 	private:
 		ref_count_data ref_counts;
-		mutable CRITICAL_SECTION  critical_section;
 
 	public:
+		HANDLE mutex;
 		reference_count()
 		{
 			ref_counts.use_count = 1;
-			InitializeCriticalSection(&critical_section);
+			mutex = CreateMutex(NULL, FALSE, NULL);
 		}
 
 		void* get_shared_ref(void* pdata)
 		{
-			EnterCriticalSection(&critical_section);
+			WaitForSingleObject(mutex, INFINITE);
 
 			if (ref_counts.use_count) {
 				ref_counts.use_count++;
-				LeaveCriticalSection(&critical_section);
+				ReleaseMutex(mutex);
 				return pdata;
 			}
 			else {
-				LeaveCriticalSection(&critical_section);
+				ReleaseMutex(mutex);
 				return NULL;
 			}
 		}
 
 		ref_count_data release_shared_ref() {
-			EnterCriticalSection(&critical_section);
-			
-
-			ref_counts.use_count--;
-			LeaveCriticalSection(&critical_section);
-			//DeleteCriticalSection(&critical_section);
+			//WaitForSingleObject(mutex, INFINITE);
+			//ref_counts.use_count--;
+			//ReleaseMutex(mutex);
 			return ref_counts;
 		}
 	};
@@ -51,44 +46,48 @@ namespace SmartPointer
 	class SafeSmartPointer
 	{
 	private:
-		T * pdata;            
-		reference_count* rc; 
+		T * pdata;
 
 	public:
-		SafeSmartPointer() : pdata(NULL), rc(NULL) {} 
+		SafeSmartPointer() : pdata(NULL), rc(NULL) {}
+		reference_count* rc;
 
-		SafeSmartPointer(T* pvalue) : pdata(pvalue), rc(NULL) {
+		SafeSmartPointer(T* pvalue) : pdata(pvalue) {
 			if (NULL != pdata) {
 				rc = new reference_count();
 			}
 		}
 
-		SafeSmartPointer(const SafeSmartPointer<T>& sp) : pdata(NULL), rc(sp.rc) {
-			if (NULL != rc) {
-				pdata = static_cast<T*>(rc->get_shared_ref(sp.pdata));
-
-				if (NULL == pdata) {
-					rc = NULL;
-				}
-			}
+		/*SafeSmartPointer(const SafeSmartPointer<T>& sp) : pdata(NULL), rc(sp.rc){
+		if (NULL != rc){
+		pdata = static_cast<T*>(rc->get_shared_ref(sp.pdata));
+		if (NULL == pdata){
+		rc = NULL;
 		}
+		}
+		}*/
 
 		~SafeSmartPointer()
 		{
 			if (NULL != rc) {
-				ref_count_data updated_counts = rc->release_shared_ref();
-
-				if (0 == updated_counts.use_count) {
-					delete pdata;
-				}
+				//CloseHandle(rc->mutex);
+				//ref_count_data updated_counts = rc->release_shared_ref();
+				//rc->~reference_count();
+				//delete rc;
+				//rc = NULL;
+				//delete pdata;
 			}
 		}
 
 		T& operator* () const {
+			WaitForSingleObject(rc->mutex, INFINITE);
+			ReleaseMutex(rc->mutex);
 			return *pdata;
 		}
 
 		T* operator-> () const {
+			WaitForSingleObject(rc->mutex, INFINITE);
+			ReleaseMutex(rc->mutex);
 			return pdata;
 		}
 
